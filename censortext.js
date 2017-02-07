2d8a467097d228082a9870f55ad775b41e06546b
Heroin <huainianshanghen@163.com>
2017-02-07 16:01:05 +0800

    all

--- censortext.js
+++ censortext.js
@@ -1,3 +1,5 @@
+var censoredWords = ["sad","bad","mad"];
+var customCensoredWords = [];
 function censor(inStr){
   for (idx in censoredWords) {
   	inStr =inStr.replace(censoredWords[idx],"*****");
